c892b0bd56093be1bd41d71f1544bcff781fab49
Kelvin Lee <kiyolee@gmail.com>
2020-05-16 00:46:12 +1000

    Update to 10.35.

--- win32/pcre2_version.h
+++ win32/pcre2_version.h
@@ -1,4 +1,4 @@
-#define PCRE2_VERSION_STR   "10.34"
+#define PCRE2_VERSION_STR   "10.35"
 #define PCRE2_VER_MAJOR     10
-#define PCRE2_VER_MINOR     34
+#define PCRE2_VER_MINOR     35
 #define PCRE2_VER_REVISION  0
