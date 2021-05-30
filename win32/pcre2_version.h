c67732817a855769d535849458988dbdaeda6788
Kelvin Lee <kiyolee@gmail.com>
2021-05-30 11:50:16 +1000

    Update to 10.37.

--- win32/pcre2_version.h
+++ win32/pcre2_version.h
@@ -1,4 +1,4 @@
-#define PCRE2_VERSION_STR   "10.36"
+#define PCRE2_VERSION_STR   "10.37"
 #define PCRE2_VER_MAJOR     10
-#define PCRE2_VER_MINOR     36
+#define PCRE2_VER_MINOR     37
 #define PCRE2_VER_REVISION  0
