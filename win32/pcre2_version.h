2ed8be05bf85de4a8fb340b01fc523893a173c3b
Kelvin Lee <kiyolee@gmail.com>
2025-08-30 08:52:09 +1000

    Update to 10.46

--- win32/pcre2_version.h
+++ win32/pcre2_version.h
@@ -1,4 +1,4 @@
-#define PCRE2_VERSION_STR   "10.45"
+#define PCRE2_VERSION_STR   "10.46"
 #define PCRE2_VER_MAJOR     10
-#define PCRE2_VER_MINOR     45
+#define PCRE2_VER_MINOR     46
 #define PCRE2_VER_REVISION  0
