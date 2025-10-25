c7d2dd8ab123b4cb3f98b528fdc14ad725ddd66d
Kelvin Lee <kiyolee@gmail.com>
2025-10-25 23:45:58 +1100

    RunGrepTest: Fix Test 132
    ".*" would go through args expansion using setargv.obj and fail the
    command.
    As replacement of 'head -1', 'pcre2grep -m1 "."' is good enough.

--- RunGrepTest.bat
+++ RunGrepTest.bat
@@ -695,7 +695,7 @@ echo RC=^%ERRORLEVEL%>>testtrygrep
 echo ---------------------------- Test 132 ----------------------------->>testtrygrep
 :: The Unix tests use fd3 here, but Windows only has StdIn/StdOut/StdErr (which, at the kernel
 :: level, are not even numbered). Use a subshell instead.
-(pushd %srcdir% & (%pcre2grep% -m1 -A3 "^match" & echo ---& %pcre2grep% -m1 ".*") <testdata/grepinput & popd) >>testtrygrep 2>&1
+(pushd %srcdir% & (%pcre2grep% -m1 -A3 "^match" & echo ---& %pcre2grep% -m1 ".") <testdata/grepinput & popd) >>testtrygrep 2>&1
 echo RC=^%ERRORLEVEL%>>testtrygrep
 
 echo ---------------------------- Test 133 ----------------------------->>testtrygrep
